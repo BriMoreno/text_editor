39bd796e68baddb371b95ac978fdcaf3480d733d
BriMoreno <BriMoreno232@gmail.com>
2022-10-29 19:04:58 -0700

    added todo's in the db js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Put to the database");
+  
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id: id, data:content});
+  const result = await request;
 
+  console.log("🚀 - data saved to the database", result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('Get from the database');
 
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  result
+    ? console.log("🚀 - data retrieved from the database", result.data)
+    : console.log("🚀 - data not found in the database");
+
+    return result?.value;
+};
 initdb();
